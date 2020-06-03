6546998b57b392b09b1dd14cae1912a698a55d22
Laize Ferraz <laizeferraz@gmail.com>
2020-06-03 16:26:01 +1000

    Made the name of the post owner a visitable link so other users can click on it to check their profiles.

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,19 +1,25 @@
-import React, { useEffect, useContext, useState } from "react";
+import React, { useEffect, useContext } from "react";
 import Axios from "axios";
 import Page from "./Page";
 import { useParams } from "react-router-dom";
 import StateContext from "../StateContext";
 import ProfilePosts from "./ProfilePosts";
+import { useImmer } from "use-immer";
 
 function Profile() {
   const { username } = useParams();
   const appState = useContext(StateContext);
-  const [profileData, setProfileData] = useState({
-    profileUsername: "...",
-    profileAvatar:
-      "https://gravatar.com/avatar/8ed140ae29a4912a1929c7a4a45885d3?s=128",
-    isFollowing: false,
-    counts: { postCount: "", folllowerCount: "", followingCount: "" },
+  const [state, setState] = useImmer({
+    followActionLoading: false,
+    startFollowingRequestCount: 0,
+    stopFollowingRequestCount: 0,
+    profileData: {
+      profileUsername: "...",
+      profileAvatar:
+        "https://gravatar.com/avatar/8ed140ae29a4912a1929c7a4a45885d3?s=128",
+      isFollowing: false,
+      counts: { postCount: "", folllowerCount: "", followingCount: "" },
+    },
   });
 
   useEffect(() => {
@@ -30,7 +36,9 @@ function Profile() {
             cancelToken: ourRequest.token,
           }
         );
-        setProfileData(response.data);
+        setState((draft) => {
+          draft.profileData = response.data;
+        });
       } catch (e) {
         console.log("There was a problem or the request was cancelled.");
       }
@@ -39,12 +47,12 @@ function Profile() {
     return () => {
       ourRequest.cancel();
     };
-  }, []);
+  }, [username]);
   return (
     <Page title="Profile Screen">
       <h2>
-        <img className="avatar-small" src={profileData.profileAvatar} />
-        {profileData.profileUsername}
+        <img className="avatar-small" src={state.profileData.profileAvatar} />
+        {state.profileData.profileUsername}
 
         <button className="btn btn-primary btn-sm ml-2">
           Follow <i className="fas fa-user-plus"></i>
@@ -53,13 +61,13 @@ function Profile() {
 
       <div className="profile-nav nav nav-tabs pt-2 mb-4">
         <a href="#" className="active nav-item nav-link">
-          Posts: {profileData.counts.postCount}
+          Posts: {state.profileData.counts.postCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Followers: {profileData.counts.folllowerCount}
+          Followers: {state.profileData.counts.folllowerCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Following: {profileData.counts.followingCount}
+          Following: {state.profileData.counts.followingCount}
         </a>
       </div>
 
