21c0a54432b890d9090ba5968b7e83ff1accf039
Laize Ferraz <laizeferraz@gmail.com>
2020-06-03 16:50:33 +1000

    Made possible to follow and unfollow users.

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -48,15 +48,116 @@ function Profile() {
       ourRequest.cancel();
     };
   }, [username]);
+
+  useEffect(() => {
+    if (state.startFollowingRequestCount) {
+      setState((draft) => {
+        draft.followActionLoading = true;
+      });
+      const ourRequest = Axios.CancelToken.source();
+
+      async function fetchData() {
+        try {
+          const response = await Axios.post(
+            `/addFollow/${state.profileData.profileUsername}`,
+            {
+              token: appState.user.token,
+            },
+            {
+              cancelToken: ourRequest.token,
+            }
+          );
+          setState((draft) => {
+            draft.profileData.isFollowing = true;
+            draft.profileData.counts.followerCount++;
+            draft.followActionLoading = false;
+          });
+        } catch (e) {
+          console.log("There was a problem or the request was cancelled.");
+        }
+      }
+      fetchData();
+      return () => {
+        ourRequest.cancel();
+      };
+    }
+  }, [state.startFollowingRequestCount]);
+
+  useEffect(() => {
+    if (state.stopFollowingRequestCount) {
+      setState((draft) => {
+        draft.followActionLoading = true;
+      });
+      const ourRequest = Axios.CancelToken.source();
+
+      async function fetchData() {
+        try {
+          const response = await Axios.post(
+            `/removeFollow/${state.profileData.profileUsername}`,
+            {
+              token: appState.user.token,
+            },
+            {
+              cancelToken: ourRequest.token,
+            }
+          );
+          setState((draft) => {
+            draft.profileData.isFollowing = false;
+            draft.profileData.counts.followerCount--;
+            draft.followActionLoading = false;
+          });
+        } catch (e) {
+          console.log("There was a problem or the request was cancelled.");
+        }
+      }
+      fetchData();
+      return () => {
+        ourRequest.cancel();
+      };
+    }
+  }, [state.stopFollowingRequestCount]);
+
+  function startFollowing() {
+    setState((draft) => {
+      draft.startFollowingRequestCount++;
+    });
+  }
+
+  function stopFollowing() {
+    setState((draft) => {
+      draft.stopFollowingRequestCount++;
+    });
+  }
   return (
     <Page title="Profile Screen">
       <h2>
         <img className="avatar-small" src={state.profileData.profileAvatar} />
         {state.profileData.profileUsername}
+        {appState.loggedIn &&
+          !state.profileData.isFollowing &&
+          appState.user.username != state.profileData.profileUsername &&
+          state.profileData.profileUsername != "..." && (
+            <button
+              onClick={startFollowing}
+              disabled={state.followActionLoading}
+              className="btn btn-primary btn-sm ml-2"
+            >
+              Follow <i className="fas fa-user-plus"></i>
+            </button>
+          )}
 
-        <button className="btn btn-primary btn-sm ml-2">
-          Follow <i className="fas fa-user-plus"></i>
-        </button>
+        {appState.loggedIn &&
+          state.profileData.isFollowing &&
+          appState.user.username != state.profileData.profileUsername &&
+          state.profileData.profileUsername != "..." && (
+            <button
+              onClick={stopFollowing}
+              disabled={state.followActionLoading}
+              className="btn btn-danger btn-sm ml-2"
+            >
+              Stop Following <i className="fas fa-user-times"></i>
+            </button>
+          )}
       </h2>
 
       <div className="profile-nav nav nav-tabs pt-2 mb-4">
@@ -64,7 +165,7 @@ function Profile() {
           Posts: {state.profileData.counts.postCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Followers: {state.profileData.counts.folllowerCount}
+          Followers: {state.profileData.counts.followerCount}
         </a>
         <a href="#" className="nav-item nav-link">
           Following: {state.profileData.counts.followingCount}
