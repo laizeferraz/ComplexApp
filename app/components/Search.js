514a826465189048386d3d2448b838b444e3af7c
Laize Ferraz <laizeferraz@gmail.com>
2020-07-23 18:13:16 +1000

    Applying lazy loading and using Suspense on Search, Chat, ViewSinglePost and CreatePost components.

--- app/components/Search.js
+++ app/components/Search.js
@@ -78,7 +78,7 @@ function Search() {
   }
 
   return (
-    <div className="search-overlay">
+    <>
       <div className="search-overlay-top shadow-sm">
         <div className="container container--narrow">
           <label htmlFor="live-search-field" className="search-overlay-icon">
@@ -141,7 +141,7 @@ function Search() {
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
